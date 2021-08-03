48d643d219d5191475dc4132ba99e63ea09f6bc5
yaobiao <1315508912@qq.com>
2021-08-03 10:45:13 +0800

    feat: 添加api验证码生成

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace think\captcha\Tests;
+
+use Mockery as m;
+use Mockery\MockInterface;
+use think\App;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase {
+    /** @var App|MockInterface */
+    protected $app;
+
+    public function tearDown(): void {
+        m::close();
+    }
+
+    protected function setUp(): void {
+        $this->app = m::mock(App::class)->makePartial();
+    }
+}
